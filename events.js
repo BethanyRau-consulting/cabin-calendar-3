947aa90ae438e42aa885fc0ae51d1271b2ebf4dd
BethanyRau-consulting <bethanyrau8@gmail.com>
2025-08-28 11:55:22 -0400

    Update events.js

--- events.js
+++ events.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB9rOOglOPQ0pzOwuFq-P_Puo9lroDPU7A",
   authDomain: "cabincalendar3.firebaseapp.com",
   projectId: "cabincalendar3",
-  storageBucket: "cabincalendar3.firebasestorage.app",
+  storageBucket: "cabincalendar3.appspot.com",
   messagingSenderId: "373184478865",
   appId: "1:373184478865:web:cf1e0e816be89107538930"
 };
