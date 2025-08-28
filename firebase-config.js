d4c42fcf5ef2bff0d03a6ca3718ff2b9b769e182
BethanyRau-consulting <bethanyrau8@gmail.com>
2025-08-28 14:05:59 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,23 @@
-const firebaseConfig = {
+(function(){
+  // Use same config everywhere
+  const firebaseConfig = {
     apiKey: "AIzaSyB9rOOglOPQ0pzOwuFq-P_Puo9lroDPU7A",
     authDomain: "cabincalendar3.firebaseapp.com",
     projectId: "cabincalendar3",
     storageBucket: "cabincalendar3.appspot.com",
     messagingSenderId: "373184478865",
     appId: "1:373184478865:web:cf1e0e816be89107538930"
-};
+  };
 
-// Initialize Firebase
-if (!firebase.apps.length) {
+  if (!window.firebase) {
+    console.error("Firebase SDK not loaded. Make sure firebase-app-compat.js is included before firebase-config.js");
+    return;
+  }
+
+  if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
-}
+  }
 
-const db = firebase.firestore();
-export { db };
+  window.db = firebase.firestore();
+  window.storage = firebase.storage();
+})();
