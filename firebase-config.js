50d8e18a89729bc89ee32067487915cf1475eb0a
BethanyRau-consulting <bethanyrau8@gmail.com>
2025-08-28 15:20:54 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,21 @@
-(function(){
-  // Use same config everywhere
-  const firebaseConfig = {
+import firebase from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js";
+import "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js";
+import "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage-compat.js";
+
+const firebaseConfig = {
     apiKey: "AIzaSyB9rOOglOPQ0pzOwuFq-P_Puo9lroDPU7A",
     authDomain: "cabincalendar3.firebaseapp.com",
     projectId: "cabincalendar3",
     storageBucket: "cabincalendar3.appspot.com",
     messagingSenderId: "373184478865",
     appId: "1:373184478865:web:cf1e0e816be89107538930"
-  };
+};
 
-  if (!window.firebase) {
-    console.error("Firebase SDK not loaded. Make sure firebase-app-compat.js is included before firebase-config.js");
-    return;
-  }
-
-  if (!firebase.apps.length) {
+if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
-  }
+}
 
-  window.db = firebase.firestore();
-  window.storage = firebase.storage();
-})();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+export { db, storage };
