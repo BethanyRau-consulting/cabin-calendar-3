958a10ee68a1df1a88202751111a3bb04fc0340c
BethanyRau-consulting <bethanyrau8@gmail.com>
2025-08-28 16:59:52 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-mport { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getFirestore,
   collection,
