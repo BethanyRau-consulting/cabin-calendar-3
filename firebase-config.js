a437bb938d76311dc905ea4ffd7695782474657e
BethanyRau-consulting <bethanyrau8@gmail.com>
2025-08-28 17:22:42 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,5 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
